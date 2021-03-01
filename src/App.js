cdab14de0fd2ca9455dfb31f038785baba2ad986
imbrikis <imbrikis@yahoo.com>
2021-02-28 22:29:53 -0700

    feat: binding event handlers

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
       plantImg: '',
       plantName: '',
       scientificName: '',
+      results: [],
     }
   }
 
@@ -27,16 +28,47 @@ class App extends Component {
           plantImg: data[2].image_url,
           plantName: data[2].common_name,
           scientificName: data[2].scientific_name,
+          results: data,
         })
-        console.log(data)
       })
+  }
+
+  search = (query) => {
+    axios
+      .get('/api/v1/plants/search', {
+        params: {
+          token: process.env.REACT_APP_API_KEY,
+          q: query,
+        },
+      })
+      .then((res) => {
+        const { data } = res.data
+        this.setState({
+          plantImg: data[0].image_url,
+          plantName: data[0].common_name,
+          scientificName: data[0].scientific_name,
+          results: data,
+        })
+      })
+  }
+
+  updateSelectedPlant = (plant) => {
+    this.setState({
+      plantImg: plant.image_url,
+      plantName: plant.common_name,
+      scientificName: plant.scientific_name,
+    })
   }
 
   render() {
     return (
       <div style={{ width: '100vw', height: '100vh' }}>
         <Header />
-        <SideNav />
+        <SideNav
+          search={this.search}
+          results={this.state.results}
+          updateSelectedPlant={this.updateSelectedPlant}
+        />
         <MainContent
           plantImage={this.state.plantImg}
           plantName={this.state.plantName}
--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,6 +1,25 @@
 import React, { Component } from 'react'
 
 export default class SideNav extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      term: '',
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      term: e.target.value,
+    })
+  }
+
+  handleSubmit = (e) => {
+    this.props.search(this.state.term)
+    e.preventDefault()
+  }
+
   render() {
     return (
       <div
@@ -11,8 +30,32 @@ export default class SideNav extends Component {
           float: 'left',
         }}
       >
-        <label>Search: </label>
-        <input type='text' />
+        <form onSubmit={this.handleSubmit}>
+          <label>Search: </label>
+          <input
+            placeholder='search for a plant'
+            value={this.state.term}
+            type='text'
+            onChange={this.handleChange}
+          />
+          <button type='submit' style={{ padding: '3px 6px' }}>
+            Submit
+          </button>
+        </form>
+        <ul>
+          {this.props.results.map((item) => {
+            return (
+              <li style={{ listStyleType: 'none' }}>
+                <a
+                  href='#'
+                  onClick={() => this.props.updateSelectedPlant(item)}
+                >
+                  {item.common_name}
+                </a>
+              </li>
+            )
+          })}
+        </ul>
       </div>
     )
   }
